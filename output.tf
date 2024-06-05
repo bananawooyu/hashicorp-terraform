b997eef57a43e1d0801d07da7df181ae41e9d3dc
unknown <jaehyeun.park@cloudnetworks.co.kr>
2024-06-05 13:51:45 +0900

    install wordpress4

--- output.tf
+++ output.tf
@@ -10,17 +10,17 @@ output "subnet_cidr" {
   value = aws_subnet.tfworkshop.cidr_block
 }
 
-output instance {
-  value = aws_instance.tfworkshop.ami
-}
+# output instance {
+#   value = aws_instance.tfworkshop.ami
+# }
 
-output "aws_instance" {
-  value = "${aws_instance.tfworkshop.id}"
-}
+# output "aws_instance" {
+#   value = "${aws_instance.tfworkshop.id}"
+# }
 
-output "wordpress_url" {
-  value = "http://${aws_eip.tfworkshop.public_ip}"
-}
+# output "wordpress_url" {
+#   value = "http://${aws_eip.tfworkshop.public_ip}"
+# }
 
 output db_instance_address {
   value = "${module.rds.db_instance_address}"
