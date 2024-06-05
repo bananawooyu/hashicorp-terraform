8eeb411f0aeeeac96fcb39be55ef0ddc0067783d
unknown <jaehyeun.park@cloudnetworks.co.kr>
2024-06-05 09:40:42 +0900

    install wordpress3

--- main.tf
+++ main.tf
@@ -33,11 +33,11 @@ resource "aws_instance" "tfworkshop" {
   vpc_security_group_ids      = [aws_security_group.tfworkshop.id]
 
   user_data = <<EOF
-  "sudo wget --no-check-certificate --no-proxy 'https://terraformworkshop-jh.s3.ap-northeast-2.amazonaws.com/wordpress.sh'"
-	"sudo chmod 777 wordpress.sh"
-	"./wordpress.sh"
-  sleep 5
-  EOF
+"sudo wget --no-check-certificate --no-proxy 'https://terraformworkshop-jh.s3.ap-northeast-2.amazonaws.com/wordpress.sh'"
+"sudo chmod 777 wordpress.sh"
+"./wordpress.sh"
+sleep 5
+EOF
 
   tags = {
     Name = "${var.prefix}-tfworkshop-instance"
